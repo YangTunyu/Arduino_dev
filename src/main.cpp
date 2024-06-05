ded6baa77824e1a5ed3a7f2be942b97eeccd1833
pang aihui <635102384@qq.com>
2024-06-05 19:24:11 +0800

    Story编号：S7018 修改人：姓名：庞爱晖 学号：202115030317 修改原因：七彩氛围灯的代码修改 业务模块：七彩氛围灯的实现

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
-#include <Arduino.h>
+#include "Freenove_WS2812_Lib_for_ESP32.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define LEDS_COUNT  8
+#define LEDS_PIN	13
+#define CHANNEL		0
+
+Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  strip.begin();
+  strip.setBrightness(20);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (int j = 0; j < 255; j += 2) {
+    for (int i = 0; i < LEDS_COUNT; i++) {
+      strip.setLedColorData(i, strip.Wheel((i * 256 / LEDS_COUNT + j) & 255));
+    }
+    strip.show();
+    delay(10);
+  }  
 }
