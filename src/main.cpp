905711d5a40c5a4e9d8b36280cfbd1e22a63a7c2
binwangn <kingmier@hotmail.com>
2024-06-05 10:20:18 +0800

    Story编号：NA 修改人：王彬 608646 修改原因：Git使用演示——修改文件 相关模块：NA

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@
 // Pin 15 can work but DHT must be disconnected during program upload.
 
 // Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
+// #define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 // See guide for details on sensor wiring and usage:
