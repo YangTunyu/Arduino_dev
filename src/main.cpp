14843ff0c3a693824048d964d8deea0836cdd43c
PanZiYang <1617149473@qq.com>
2024-06-06 19:01:00 +0800

    Story编号：S7058 S7017 修改人：姓名：潘子扬 学号：202115030316 修改原因：重新合并代码 业务模块：人体感应自动开灯，LED灯无极调光

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,15 @@ const int alarmThreshold = 1; // 数字输入阈值
 
 
 
+//pzy
+// 定义红外人体感应模块和LED灯的引脚
+const int sensorPin = 14; // 人体感应模块的输出引脚连接到GPIO22
+const int ledPin = 16;     // LED灯连接到GPIO13
+
+int potpin= 15;//定义模拟ADC接口15
+int ledpin= 16;//定义数字接口16（PWM 输出）
+int val=0;// 暂存来自传感器的变量数值
+
 
 
 
@@ -97,6 +106,17 @@ void setup() {
   pinMode(buzzerPin, OUTPUT); 
   digitalWrite(buzzerPin, HIGH);
   // 设置蜂鸣器引脚为输出模式
+
+
+
+  //pzy
+pinMode(sensorPin, INPUT); // 设置人体感应模块引脚为输入
+  pinMode(ledPin, OUTPUT);   // 设置LED灯引脚为输出
+
+  pinMode(ledpin,OUTPUT);//定义数字接口11 为输出
+Serial.begin(9600);//设置波特率为9600
+//模拟接口自动设置为输入
+
 }
 
 
@@ -278,4 +298,22 @@ void loop() {
   // }
 
   delay(1000); // 延迟1秒钟
+
+
+
+
+
+//pzy
+  if (digitalRead(sensorPin) == HIGH) {
+    digitalWrite(ledPin, HIGH); // 如果检测到人体，点亮LED灯
+  } else {
+    digitalWrite(ledPin, LOW);  // 否则，熄灭LED灯
+  }
+
+
+  val=analogRead(potpin);// 读取传感器的模拟值并赋值给val  读取模拟值范围 0-1023
+Serial.println(val);//显示val 变量  用来串口监视
+
+analogWrite(ledpin,val/4);// 打开LED 并设置亮度（PWM 输出最大值255）
+delay(10);//延时0.01 秒
 }
