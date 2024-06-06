64401f96615c9463f68124bb0d8db4d0ad6df0aa
yanxinru <1085583442@qq.com>
2024-06-06 16:27:46 +0800

    Story编号：S7010 添加人员：演鑫如 202115010224 添加原因：添加代码 业务模块：可燃气体泄露告警

--- src/main.cpp
+++ src/main.cpp
@@ -818,3 +818,45 @@ void loop()
 {
   // do nothing
 }
+
+
+//可燃气体泄露告警
+#include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightSensor;
+const int ledPin = 16; // LED的控制引脚连接到GPIO 16
+const int pirPin = 14; // HC-SR312微型人体感应模块的信号引脚连接到GPIO 14
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+
+  Wire.begin(21, 22); // 初始化I2C总线，SDA连接到GPIO 21，SCL连接到GPIO 22
+  lightSensor.begin();
+}
+
+void loop() {
+  // 读取光线强度
+  uint16_t lux = lightSensor.readLightLevel();
+  Serial.print("Light level (lux): ");
+  Serial.println(lux);
+
+  // 读取人体感应模块状态
+  int motionDetected = digitalRead(pirPin);
+  Serial.print("People Condition: ");
+  Serial.println(motionDetected);
+  
+
+  // 当光线暗且有人时，点亮LED
+  if (lux < 500 && motionDetected == HIGH) {
+    digitalWrite(ledPin, HIGH); // 点亮LED
+    Serial.print("Light Condition: HIGH "); 
+  } else {
+    digitalWrite(ledPin, LOW); // 关闭LED
+    Serial.print("Light Condition: LOW ");
+  }
+  delay(1000); // 延迟1秒钟
+}
