478a2e5c7751a370c2064ce8c1b3b95b5f430299
ZhangBinyue <2695805956@qq.com>
2024-06-06 11:54:04 +0800

    Story编号：S7008 修改人：张缤月 202115030306 修改原因：代码提交 业务模块：MQ2烟雾传感器

--- src/main.cpp
+++ src/main.cpp
@@ -554,3 +554,53 @@ void loop()
     Serial.println(F("%"));
   }
 }
+
+// 烟雾浓度数据获取
+#include <MQUnifiedsensor.h>
+
+//Definitions
+#define placa "Arduino UNO"
+#define Voltage_Resolution 5
+#define pin 13 //Analog input 4 of your arduino
+#define type "MQ-2" //MQ4
+#define ADC_Bit_Resolution 10 // For arduino UNO/MEGA/NANO
+#define RatioMQ4CleanAir 4.4  //RS / R0 = 4.4 ppm 
+//#define calibration_button 13 //Pin to calibrate your sensor
+
+//Declare Sensor
+MQUnifiedsensor MQ2(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+
+void setup() {
+  //Init serial port
+  Serial.begin(115200);
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ2.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ2.setA(30000000); MQ2.setB(-8.308); // Configure the equation to to calculate CH4 concentration
+
+  MQ2.init(); 
+
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ2.update(); // Update data, the arduino will read the voltage from the analog pin
+    calcR0 += MQ2.calibrate(RatioMQ4CleanAir);
+    Serial.print(".");
+  }
+  MQ2.setR0(calcR0/10);
+  Serial.println("  done!.");
+  
+  if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
+  if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
+  /*****************************  MQ CAlibration ********************************************/ 
+  MQ2.serialDebug(true);
+}
+
+  void loop() {
+
+  MQ2.update();
+  float smokePPM = MQ2.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
+  if(smokePPM > 1000) {Serial.println("Warning: High concentrations of smoke detected");}
+  MQ2.serialDebug(); // Will print the table on the serial port
+  delay(400);  
+}
